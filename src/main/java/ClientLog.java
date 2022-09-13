9250676a6b5e8b3c06a13b493af5483c9af949ca
Sylaman <Sylaman.snake@gmail.com>
2022-09-13 19:13:57 +0300

    Maven and Open CSV added

---
+++ src/main/java/ClientLog.java
@@ -0,0 +1,12 @@
+import java.io.File;
+
+public class ClientLog {
+
+    public void log(int productNum, int amount) {
+
+    }
+
+    public void exportAsCSV(File txtFile) {
+
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,53 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("basket.txt");
+
+        String[] products = new String[]{"Яблоко", "Помидор", "Апельсин", "Груша"};
+        int[] prices = new int[]{30, 50, 70, 40};
+        Basket basket = new Basket(products, prices);
+        ClientLog clientLog = new ClientLog();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Список возможных товаров для покупки");
+        for (int i = 0; i < basket.getProducts().length; i++) {
+            String product = basket.getProducts()[i];
+            int price = basket.getPrices()[i];
+            int position = i + 1;
+            System.out.println(position + ". " + product + " " + price + " руб/шт");
+        }
+        if (!file.createNewFile()) {
+            basket = Basket.loadFromTxtFile(file);
+            basket.printCart();
+        }
+
+        while (true) {
+            System.out.println("Выберите товар и количество или введите `end`");
+            String input = scanner.nextLine();
+            if (input.equals("end")) {
+                break;
+            }
+            String[] inputArr = input.split(" ");
+            if (inputArr.length != 2) {
+                System.out.println("Ошибка ввода! Должно быть введено 2 числа!");
+                continue;
+            }
+            int productNum = Integer.parseInt(inputArr[0]) - 1;
+            int amount = Integer.parseInt(inputArr[1]);
+            try {
+                basket.addToCart(productNum, amount);
+            } catch (ArrayIndexOutOfBoundsException exception) {
+                System.out.println("Товар с таким номером не найден!");
+                continue;
+            }
+            clientLog.log(productNum, amount);
+            basket.printCart();
+            basket.saveTxt(file);
+        }
+    }
+}
